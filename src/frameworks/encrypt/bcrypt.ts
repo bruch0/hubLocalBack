f24c23ffea766f732f8f1ec809260c9a9d3bda12
bruch0 <lucas.bruch0@gmail.com>
2023-03-15 17:38:15 -0300

    feat: implement encryption with bcrypt

---
+++ src/frameworks/encrypt/bcrypt.ts
@@ -0,0 +1,10 @@
+import bcrypt from 'bcrypt';
+
+import { GenericEncrypter } from '@data-service';
+
+export class EncryptService implements GenericEncrypter {
+  encrypt = (data: string) => bcrypt.hashSync(data, 12);
+
+  compare = (data: string, hashData: string) =>
+    bcrypt.compareSync(data, hashData);
+}
