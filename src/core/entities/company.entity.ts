793a7070d4845d7f18348745f1aee8f78860a67d
bruch0 <lucas.bruch0@gmail.com>
2023-03-15 12:52:26 -0300

    feat: create app entities

---
+++ src/core/entities/company.entity.ts
@@ -0,0 +1,8 @@
+export class Company {
+  id: number;
+  name: string;
+  siteUrl: string;
+  taxId: string;
+  userId: number;
+  deleted: boolean;
+}
---
+++ src/core/entities/local.entity.ts
@@ -0,0 +1,12 @@
+export class Local {
+  id: number;
+  name: string;
+  zipcode: string;
+  state: string;
+  city: string;
+  neighborhood: string;
+  streetAddress: string;
+  number?: number;
+  companyId: number;
+  deleted: boolean;
+}
