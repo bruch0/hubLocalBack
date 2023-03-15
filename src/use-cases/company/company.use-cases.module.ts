7fc61b47f25c97ccc52b69f63b865b420cbe5a11
bruch0 <lucas.bruch0@gmail.com>
2023-03-15 17:52:39 -0300

    feat: create company module

---
+++ src/use-cases/company/company.use-cases.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { DatabaseService } from '@database/prisma';
+
+import { CompanyFactoryService } from './company.use-cases.service';
+import { CompanyUseCases } from './company.use-cases';
+
+@Module({
+  providers: [DatabaseService, CompanyFactoryService, CompanyUseCases],
+  exports: [CompanyFactoryService, CompanyUseCases],
+})
+export class CompanyUseCasesModule {}
