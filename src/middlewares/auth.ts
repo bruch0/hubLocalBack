5bd1bab4daf1099ae6c891744184b2ce90e68d7c
bruch0 <lucas.bruch0@gmail.com>
2023-03-15 19:49:36 -0300

    feat: create auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,27 @@
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+import { AuthService } from '@auth/jwt';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  authService = new AuthService();
+  use(request: Request, response: Response, next: NextFunction) {
+    const authHeader = request.headers.authorization;
+    if (authHeader == null || !/Bearer [a-zA-Z0-9]/.test(authHeader))
+      throw new UnauthorizedException('Você precisa estar logado');
+
+    const token = authHeader.replace('Bearer ', '');
+
+    const validToken = this.authService.verify(token);
+
+    if (!validToken)
+      throw new UnauthorizedException('Você precisa estar logado');
+
+    next();
+  }
+}
