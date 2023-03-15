3ffc4d8af6681ee1d628fcfb90c70d6837c588a6
bruch0 <lucas.bruch0@gmail.com>
2023-03-15 17:52:31 -0300

    feat: create company usecase file

---
+++ src/use-cases/company/company.use-cases.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+
+import { DatabaseService } from '@database/prisma';
+
+import { CreateCompanyDto, UpdateCompanyDto, DeleteCompanyDto } from '@dtos';
+
+import { CompanyFactoryService } from './company.use-cases.service';
+
+@Injectable()
+export class CompanyUseCases {
+  constructor(
+    private databaseService: DatabaseService,
+    private companyFactoryService: CompanyFactoryService,
+  ) {}
+
+  async createCompany(companyData: CreateCompanyDto): Promise<void> {
+    const newCompany = this.companyFactoryService.createCompany(companyData);
+
+    await this.databaseService.createCompany(newCompany);
+  }
+
+  async updateCompany(companyData: UpdateCompanyDto): Promise<void> {
+    const company = this.companyFactoryService.updateCompany(companyData);
+
+    await this.databaseService.createCompany(company);
+  }
+
+  async deleteCompany(companyData: DeleteCompanyDto): Promise<void> {
+    const company = this.companyFactoryService.deleteCompany(companyData);
+
+    await this.databaseService.createCompany(company);
+  }
+}
