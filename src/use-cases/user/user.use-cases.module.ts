bbc9bb07e7827a5f6a8f140151078e61b661cdec
bruch0 <lucas.bruch0@gmail.com>
2023-03-15 17:37:20 -0300

    feat: create user module

---
+++ src/use-cases/user/user.use-cases.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+
+import { DatabaseService } from '@database/prisma';
+import { EncryptService } from '@encrypt/bcrypt';
+import { AuthService } from '@auth/jwt';
+
+import { UserFactoryService } from './user.use-cases.service';
+import { UserUseCases } from './user.use-cases';
+
+@Module({
+  providers: [
+    DatabaseService,
+    EncryptService,
+    AuthService,
+    UserFactoryService,
+    UserUseCases,
+  ],
+  exports: [UserFactoryService, UserUseCases],
+})
+export class UserUseCasesModule {}
