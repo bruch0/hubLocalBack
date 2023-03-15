58e5b07b72aa773f3cb7547be08fa9b16b38fcff
bruch0 <lucas.bruch0@gmail.com>
2023-03-15 19:20:29 -0300

    feat: improve company use case

--- src/use-cases/company/company.use-cases.ts
+++ src/use-cases/company/company.use-cases.ts
@@ -1,6 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 
 import { DatabaseService } from '@database/prisma';
+
+import { ResponseCompany } from '@entities';
 
 import { CreateCompanyDto, UpdateCompanyDto, DeleteCompanyDto } from '@dtos';
 
@@ -13,16 +19,35 @@ export class CompanyUseCases {
     private companyFactoryService: CompanyFactoryService,
   ) {}
 
-  async createCompany(companyData: CreateCompanyDto): Promise<void> {
+  async createCompany(companyData: CreateCompanyDto): Promise<ResponseCompany> {
     const newCompany = this.companyFactoryService.createCompany(companyData);
 
-    await this.databaseService.createCompany(newCompany);
+    const company = await this.databaseService.getCompany({
+      taxId: newCompany.taxId,
+    });
+
+    if (company) throw new ConflictException('CNPJ já cadastrado');
+
+    return await this.databaseService.createCompany(newCompany);
   }
 
-  async updateCompany(companyData: UpdateCompanyDto): Promise<void> {
+  async updateCompany(companyData: UpdateCompanyDto): Promise<ResponseCompany> {
     const company = this.companyFactoryService.updateCompany(companyData);
 
-    await this.databaseService.updateCompany({
+    const validCompany = await this.databaseService.getCompany({
+      id: company.id,
+    });
+
+    if (!validCompany) throw new NotFoundException('Empresa não existente');
+
+    const invalidTaxId = await this.databaseService.getCompany({
+      taxId: company.taxId,
+    });
+
+    if (invalidTaxId && company.id !== invalidTaxId.id)
+      throw new ConflictException('CNPJ já cadastrado');
+
+    return await this.databaseService.updateCompany({
       ...company,
       companyId: company.id,
     });
