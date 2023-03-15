bb7877a095b396961cb53bab5a1de28be190e703
bruch0 <lucas.bruch0@gmail.com>
2023-03-15 17:37:10 -0300

    feat: create use case file implementing the methods

---
+++ src/use-cases/user/user.use-cases.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+
+import { DatabaseService } from '@database/prisma';
+import { EncryptService } from '@encrypt/bcrypt';
+import { AuthService } from '@auth/jwt';
+
+import { CreateUserDto, LoginUserDto } from '@dtos';
+
+import { UserFactoryService } from './user.use-cases.service';
+
+@Injectable()
+export class UserUseCases {
+  constructor(
+    private databaseService: DatabaseService,
+    private encryptService: EncryptService,
+    private authService: AuthService,
+    private userFactoryService: UserFactoryService,
+  ) {}
+
+  async createUser(userData: CreateUserDto): Promise<void> {
+    const newNote = this.userFactoryService.createNewUser(userData);
+
+    newNote.password = this.encryptService.encrypt(newNote.password);
+
+    await this.databaseService.createUser(newNote);
+  }
+
+  async loginUser(userLoginData: LoginUserDto): Promise<string> {
+    const userData = this.userFactoryService.loginUser(userLoginData);
+
+    const user = await this.databaseService.findUser(userData);
+
+    const passwordMatch = this.encryptService.compare(
+      userData.password,
+      user.password,
+    );
+
+    if (!passwordMatch) throw new Error('Invalid password');
+
+    return this.authService.sign({ email: user.email, id: user.id });
+  }
+}
