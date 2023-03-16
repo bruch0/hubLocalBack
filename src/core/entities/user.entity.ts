08f663cad276a25a6450c64350d6b177f13a4623
bruch0 <lucas.bruch0@gmail.com>
2023-03-15 23:04:37 -0300

    feat: update user factory

--- src/core/entities/user.entity.ts
+++ src/core/entities/user.entity.ts
@@ -1,4 +1,16 @@
+type UserDto = {
+  name?: string;
+  email?: string;
+  password?: string;
+};
+
 export class User {
+  constructor(createUserDto: UserDto) {
+    this.name = createUserDto.name;
+    this.email = createUserDto.email;
+    this.password = createUserDto.password;
+  }
+
   id: number;
   name: string;
   email: string;
--- src/use-cases/user/user.use-cases.factory.ts
+++ src/use-cases/user/user.use-cases.factory.ts
@@ -7,20 +7,13 @@ import { CreateUserDto, LoginUserDto } from '@dtos';
 @Injectable()
 export class UserFactoryService {
   createUser(createUserDto: CreateUserDto) {
-    const newUser = new User();
-
-    newUser.name = createUserDto.name;
-    newUser.email = createUserDto.email;
-    newUser.password = createUserDto.password;
+    const newUser = new User(createUserDto);
 
     return newUser;
   }
 
-  loginUser(updateCompanyDto: LoginUserDto) {
-    const user = new User();
-
-    user.email = updateCompanyDto.email;
-    user.password = updateCompanyDto.password;
+  loginUser(updateUserDto: LoginUserDto) {
+    const user = new User(updateUserDto);
 
     return user;
   }
