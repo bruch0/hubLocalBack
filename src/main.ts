b730747a888ccab50f2890946ebfc0779e8e9050
bruch0 <lucas.bruch0@gmail.com>
2023-03-16 10:09:45 -0300

    fix: add dotenv config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  dotenv.config({ path: process.env.ENV_FILE_PATH });
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
+
 bootstrap();
