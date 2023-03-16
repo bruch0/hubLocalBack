a09b8ec5e6730e9aa64eb006b817421475b831c9
bruch0 <lucas.bruch0@gmail.com>
2023-03-16 11:14:33 -0300

    test: add company find and create tests

--- test/frameworks/database.spec.ts
+++ test/frameworks/database.spec.ts
@@ -1,6 +1,11 @@
 import { DatabaseService } from '@database/prisma';
 
-import { generateCreateUserDto, generateFindUserDto } from './database.utils';
+import {
+  generateCreateCompanyDto,
+  generateCreateUserDto,
+  generateFindCompanyDto,
+  generateFindUserDto,
+} from './database.utils';
 
 class testDatabaseService extends DatabaseService {
   rawQuery = async (query: string) => this.prismaService.$queryRawUnsafe(query);
@@ -26,14 +31,6 @@ describe('Database Service', () => {
     expect(result).toEqual({ ...createUserDto, id: expect.any(Number) });
   });
 
-  it('Should create and return the user email when the email is not taken', async () => {
-    const createUserDto = generateCreateUserDto();
-
-    const result = await databaseService.createUser(createUserDto);
-
-    expect(result).toEqual({ email: createUserDto.email });
-  });
-
   it('Should throw an error when the email is taken', async () => {
     const createUserDto = generateCreateUserDto();
 
@@ -42,5 +39,49 @@ describe('Database Service', () => {
     expect(async () => {
       await databaseService.createUser(createUserDto);
     }).rejects.toThrow();
+  });
+
+  it('Should create and return the user email when the email is not taken', async () => {
+    const createUserDto = generateCreateUserDto();
+
+    const result = await databaseService.createUser(createUserDto);
+
+    expect(result).toEqual({ email: createUserDto.email });
+  });
+
+  it('Should return null when looking for an inexistent company', async () => {
+    const findCompanyDto = generateFindCompanyDto();
+
+    const result = await databaseService.findCompany(findCompanyDto);
+
+    expect(result).toEqual(null);
+  });
+
+  it('Should return a company when looking for an existent company', async () => {
+    const createCompanyDto = generateCreateCompanyDto();
+    createCompanyDto.userId = 1;
+    const company = await databaseService.createCompany(createCompanyDto);
+
+    const result = await databaseService.findCompany({ taxId: company.taxId });
+
+    expect(result).toEqual({ ...createCompanyDto, id: expect.any(Number), deleted: false });
+  });
+
+  it('Should throw an error when the userId is invalid', async () => {
+    const createCompanyDto = generateCreateCompanyDto();
+
+    expect(async () => {
+      await databaseService.createCompany(createCompanyDto);
+    }).rejects.toThrow();
+  });
+
+  it('Should create and return the company data when the userId is valid', async () => {
+    const createCompanyDto = generateCreateCompanyDto();
+    createCompanyDto.userId = 1;
+
+    const result = await databaseService.createCompany(createCompanyDto);
+
+    delete createCompanyDto.userId;
+    expect(result).toEqual(createCompanyDto);
   });
 });
