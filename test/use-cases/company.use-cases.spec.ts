1c3ca478c6757d997380f07a2a67267a5e7171b3
bruch0 <lucas.bruch0@gmail.com>
2023-03-16 14:46:14 -0300

    test: add company use-cases tests

---
+++ test/use-cases/company.use-cases.spec.ts
@@ -0,0 +1,139 @@
+import { TestingModule, Test } from '@nestjs/testing';
+import * as faker from 'faker';
+
+import { DatabaseService } from '@database/prisma';
+
+import { AuthService } from '@auth/jwt';
+
+import { EncryptService } from '@encrypt/bcrypt';
+
+import {
+  CreateCompanyDto,
+  CreateUserDto,
+  DeleteCompanyDto,
+  GetUserCompaniesDto,
+  LoginUserDto,
+  UpdateCompanyDto,
+} from '@dtos';
+
+import { CompanyUseCases } from '@company/company.use-cases';
+import { CompanyFactoryService } from '@company/company.use-cases.factory';
+
+describe('Company Usecases', () => {
+  let app: TestingModule;
+  let companyUseCases: CompanyUseCases;
+
+  const mockedCompanyFactoryService: CompanyFactoryService = {
+    createCompany: (data) => data,
+    updateCompany: (data) => data,
+    deleteCompany: (data) => data,
+  };
+
+  const mockedDatabaseService = {
+    findUser: () => null,
+    createUser: () => null,
+    getUserCompanies: () => null,
+    findCompany: () => null,
+    createCompany: () => null,
+    updateCompany: () => null,
+    deleteCompany: () => null,
+    getCompanyLocals: () => null,
+    findLocal: () => null,
+    createLocal: () => null,
+    updateLocal: () => null,
+    deleteLocal: () => null,
+  };
+
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      providers: [DatabaseService, CompanyFactoryService, CompanyUseCases],
+    })
+      .overrideProvider(DatabaseService)
+      .useValue(mockedDatabaseService)
+      .overrideProvider(CompanyFactoryService)
+      .useValue(mockedCompanyFactoryService)
+      .compile();
+
+    companyUseCases = app.get(CompanyUseCases);
+  });
+
+  it('Should call the database and factory methods and return the database response', async () => {
+    const databaseResponse = [];
+
+    jest
+      .spyOn(mockedDatabaseService, 'getUserCompanies')
+      .mockImplementationOnce(() => databaseResponse);
+
+    const getUserCompaniesDto: GetUserCompaniesDto = {
+      userId: faker.datatype.number(),
+    };
+
+    const result = await companyUseCases.getUserCompanies(getUserCompaniesDto);
+
+    expect(mockedDatabaseService.getUserCompanies).toHaveBeenCalled();
+    expect(result).toEqual([]);
+  });
+
+  it('Should throw an error if provided userId returns no user', async () => {
+    jest.spyOn(mockedDatabaseService, 'findUser').mockImplementationOnce(() => null);
+
+    const createCompanyDto: CreateCompanyDto = {
+      name: faker.name.findName(),
+      siteUrl: faker.internet.url(),
+      taxId: faker.helpers.regexpStyleStringParse(
+        '[01-99].[001-999].[001-999]/[0001-9999]-[01-99]',
+      ),
+      userId: faker.datatype.number(),
+    };
+
+    expect(async () => {
+      await companyUseCases.createCompany(createCompanyDto);
+    }).rejects.toThrow('Usuário não existe');
+  });
+
+  it('Should throw an error if provided taxId is already registered', async () => {
+    jest.spyOn(mockedDatabaseService, 'findUser').mockImplementationOnce(() => true);
+    jest.spyOn(mockedDatabaseService, 'findCompany').mockImplementationOnce(() => true);
+
+    const createCompanyDto: CreateCompanyDto = {
+      name: faker.name.findName(),
+      siteUrl: faker.internet.url(),
+      taxId: faker.helpers.regexpStyleStringParse(
+        '[01-99].[001-999].[001-999]/[0001-9999]-[01-99]',
+      ),
+      userId: faker.datatype.number(),
+    };
+
+    expect(async () => {
+      await companyUseCases.createCompany(createCompanyDto);
+    }).rejects.toThrow('CNPJ já cadastrado');
+  });
+
+  it('Should call the database and factory methods and return the database response', async () => {
+    const databaseResponse = { id: faker.datatype.number() };
+
+    jest.spyOn(mockedDatabaseService, 'findUser').mockImplementationOnce(() => true);
+    jest.spyOn(mockedDatabaseService, 'findCompany').mockImplementationOnce(() => false);
+    jest
+      .spyOn(mockedDatabaseService, 'createCompany')
+      .mockImplementationOnce(() => databaseResponse);
+    jest.spyOn(mockedCompanyFactoryService, 'createCompany');
+
+    const createCompanyDto: CreateCompanyDto = {
+      name: faker.name.findName(),
+      siteUrl: faker.internet.url(),
+      taxId: faker.helpers.regexpStyleStringParse(
+        '[01-99].[001-999].[001-999]/[0001-9999]-[01-99]',
+      ),
+      userId: faker.datatype.number(),
+    };
+
+    const result = await companyUseCases.createCompany(createCompanyDto);
+
+    expect(mockedDatabaseService.findUser).toHaveBeenCalled();
+    expect(mockedDatabaseService.findCompany).toHaveBeenCalled();
+    expect(mockedDatabaseService.createCompany).toHaveBeenCalled();
+    expect(mockedCompanyFactoryService.createCompany).toHaveBeenCalled();
+    expect(result).toEqual(databaseResponse);
+  });
+});
